b8c4d4a30b06f1cfb7c74bdeaef003fa3f7db21b
cssun <sathwiksankesha@gmail.com>
2023-03-10 21:45:08 +0530

    sacc

--- app/src/main/java/com/cssun/flcalendar/Date_Dif.kt
+++ app/src/main/java/com/cssun/flcalendar/Date_Dif.kt
@@ -1,11 +1,14 @@
 package com.cssun.flcalendar
 
 import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import java.util.*
 
 class Date_Dif : AppCompatActivity() {
@@ -13,6 +16,7 @@ class Date_Dif : AppCompatActivity() {
     private lateinit var endDatePicker: DatePicker
     private lateinit var calculateButton: Button
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,17 @@ class Date_Dif : AppCompatActivity() {
         startDatePicker = findViewById(R.id.start_date_picker)
         endDatePicker = findViewById(R.id.end_date_picker)
         calculateButton = findViewById(R.id.calculate_button)
+
+
+
+        startDatePicker.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            view.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+        }
+
+        endDatePicker.setOnDateChangedListener { view, year, monthOfYear, dayOfMonth ->
+            view.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+        }
+
 
         calculateButton.setOnClickListener {
             val startYear = startDatePicker.year
@@ -38,7 +53,7 @@ class Date_Dif : AppCompatActivity() {
             endCalendar.set(endYear, endMonth, endDay)
 
             val diffInMillis = endCalendar.timeInMillis - startCalendar.timeInMillis
-            val diffInDays = diffInMillis / (1000 * 60 * 60 * 24)
+            val diffInDays = Math.abs(diffInMillis / (1000 * 60 * 60 * 24))
 
             val message = "The difference between the selected dates is $diffInDays days."
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
